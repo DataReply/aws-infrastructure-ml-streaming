5ac5d07562e980328a0c63c959e76fb25f12dd43
Tayfun Wiechert <t.wiechert@reply.de>
2021-05-01 17:43:17 +0200

    working version

--- workspaces/k3s/main.tf
+++ workspaces/k3s/main.tf
@@ -23,8 +23,8 @@ module "vpc" {
   enable_dns_support   = true
 
   # optionally,
-  enable_nat_gateway = false
-  single_nat_gateway = false # perhaps you got money to burn?
+  enable_nat_gateway = true
+  single_nat_gateway = true # perhaps you got money to burn?
 
   tags = var.tags
 }
@@ -59,14 +59,3 @@ module "k3s-in-new-vpc" {
 }
 
 
- # If you prefer to use spot-instances for NAT, use this
- # You don't need this if nat_gateway is enabeld
- module "nat" {
-   source = "int128/nat-instance/aws"
-
-   name                        = "k3s_nat"
-   vpc_id                      = module.vpc.vpc_id
-   public_subnet               = module.vpc.public_subnets[0]
-   private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
-   private_route_table_ids     = module.vpc.private_route_table_ids
- }
