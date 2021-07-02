688f49d3bf3620d0d2603c0d76a2ebec3bb85e27
Tayfun Wiechert <t.wiechert@reply.de>
2021-07-02 09:06:05 +0200

    settin up bootstrap routine for arm cluster

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+CLUSTER_NAME=${1:-k3s-cluster}
 
-INSTANCE_ID=$(aws ec2 describe-instances --profile "datareply" --filters "Name=tag:k3s-role,Values=master" | jq '.Reservations[0].Instances[0].InstanceId' -r)
+INSTANCE_ID=$(aws ec2 describe-instances --profile "datareply" --filters "Name=tag:Name,Values=$CLUSTER_NAME-master" | jq '.Reservations[0].Instances[0].InstanceId' -r)
 COMMAND_ID=$(aws ssm send-command --profile "datareply"  \
     --document-name "AWS-RunShellScript" \
     --parameters 'commands=["cat /etc/rancher/k3s/k3s.yaml"]' \
@@ -9,5 +10,5 @@ sleep 10
 
 aws ssm  get-command-invocation --profile "datareply" \
 --command-id $COMMAND_ID \
---instance-id $INSTANCE_ID | jq '.StandardOutputContent' -r > k3s.yaml
-export KUBECONFIG=$(PWD)/k3s.yaml
+--instance-id $INSTANCE_ID | jq '.StandardOutputContent' -r > $CLUSTER_NAME.yaml
+export KUBECONFIG=$(PWD)/$CLUSTER_NAME.yaml
