8865b7610ba6a98389061c64b390ee1f2129f449
m.schmoeller <m.schmoeller@reply.de>
2022-06-13 13:36:12 +0200

    fixed bootstrap script

--- bin/connect.sh
+++ bin/connect.sh
@@ -3,7 +3,7 @@ CLUSTER_NAME=${1:-k3s-cluster}
 
 export KUBECONFIG=$(PWD)/$CLUSTER_NAME.yaml
 
-INSTANCE_ID=$(aws ec2 describe-instances --profile "datareply" --filters "Name=tag:Name,Values=$CLUSTER_NAME-master" | jq '.Reservations[0].Instances[0].InstanceId' -r)
+INSTANCE_ID=$(aws ec2 describe-instances --profile "datareply" --filters Name=tag:Name,Values=k3s-cluster-master Name=instance-state-name,Values=running | jq '.Reservations[0].Instances[0].InstanceId' -r)
 aws ssm start-session --profile datareply  \
     --target $INSTANCE_ID  \
     --document-name AWS-StartPortForwardingSession \
